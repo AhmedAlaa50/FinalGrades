94ef40ef55502c62e14d95a00849ea6385c9a4d8
Ahmed Alaa <85447944+AhmedAlaa50@users.noreply.github.com>
2022-02-23 14:40:32 +0200

    Edited script.js and index.html
    Created the table for the students with random marks (id still not handeled)

--- script.js
+++ script.js
@@ -4,6 +4,53 @@ const number_of_students = document.querySelector("#students-count");
 const total_average = document.querySelector("#total-average");
 const records = document.querySelector("#records");
 
+let students_records = [];
+
 slider.oninput = () => {
     number_of_students.innerHTML = slider.value;
 };
+
+btn.onclick = () => {
+    records.innerHTML =  `
+        <li id="table-fields">
+                <span>ID</span>
+                <span>Name</span>
+                <span>Math</span>
+                <span>Physics</span>
+                <span>Chemistry</span>
+                <span>History</span>
+                <span>English</span>
+                <span>Average</span>
+            </li>
+    `;
+    for(i = 0; i < slider.value; i++) {
+        students_records.push({
+            id: "0",
+            name: `${Math.round(Math.random()) === 0 ? names.male[Math.floor(Math.random() * 100)] : names.female[Math.floor(Math.random() * 100)]} ${names.surnames[Math.floor(Math.random() * 300)]}`,
+            math: Math.floor(Math.random() * 100),
+            physics: Math.floor(Math.random() * 100),
+            chemistry: Math.floor(Math.random() * 100),
+            history: Math.floor(Math.random() * 100),
+            english: Math.floor(Math.random() * 100),
+            average: 0
+        })
+    }
+    
+    students_records.forEach((student) => {
+        student.average = (student.math + student.physics + student.chemistry + student.history + student.english) / 5;
+    })
+    records.innerHTML += students_records.reduce((acc, record) => {
+        return acc + `<li>
+        <span>${record.id}</span>
+        <span>${record.name}</span>
+        <span>${record.math}</span>
+        <span>${record.physics}</span>
+        <span>${record.chemistry}</span>
+        <span>${record.history}</span>
+        <span>${record.english}</span>
+        <span>${record.average}</span>
+        </li>`;
+    })
+};
+
+//sort reduce map forEach
